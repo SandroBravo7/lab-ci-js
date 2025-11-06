aebb82dfc77f222b2fffd6e2d30299460df8ef63
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2025-11-06 11:36:42 -0500

    fix: Separa código de funciones y pruebas, y completa feat/division

--- math.test.js
+++ math.test.js
@@ -1,4 +1,4 @@
-const { suma, resta, multiplica } = require('./math');
+const { suma, resta, multiplica, divide } = require('./math');
 
 test('suma de 2 + 3 debe ser 5', () => {
     expect(suma(2, 3)).toBe(5);
@@ -10,4 +10,13 @@ test('resta de 5 - 2 debe ser 3', () => {
 
 test('multiplicación de 3 * 4 debe ser 12', () => {
     expect(multiplica(3, 4)).toBe(12);
+});
+
+test('divide de 40 / 8 debe ser 5', () => {
+    expect(divide(40, 8)).toBe(5);
+});
+
+// **RECOMENDADO:** Añadir el test para división por cero
+test('manejo de división por cero', () => {
+    expect(() => divide(10, 0)).toThrow('No se puede dividir por cero.');
 });
